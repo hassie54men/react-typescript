1864ed8e16d0dbb73a307042b7a2fc7c996ec14f
Danil <hassie54men@icloud.com>
2025-08-17 23:55:48 +0300

    сделал что то наподобии туду, конвертер валют закоментировал на время

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import Button from "./components/Button.tsx";
+import ToDo from "./components/ToDo/ToDo.tsx";
 
 export default function App() {
-  const apiConfig = {
-    key: 'fca_live_fCuXdLSDeCeIwwJU0sLqTCutBkrRv0Mp2fGWJH9J',
-    url: 'https://api.freecurrencyapi.com/v1/',
-  };
-
-  const [valueBase, setValueBase] = useState('')
-  const [valueRub, setValueRub] = useState('')
-  const [valute, setValute] = useState<Record<string, number> | null>(null);
-  const [rub, setRub] = useState<number | null>(null);
-  const [show, setShow] = useState(false)
-
- function changeValueBase(event: React.ChangeEvent<HTMLInputElement>) {
-   setValueBase(event.target.value.toUpperCase())
-  }
-  function changeValueRub(event: React.ChangeEvent<HTMLInputElement>) {
-    setValueRub(event.target.value.toUpperCase())
-  }
-
-  async function apiValute(){
-    const response = await fetch(`${apiConfig.url}latest?apikey=${apiConfig.key}&base_currency=${valueBase}`)
-    const data = await response.json()
-    console.log(data.data)
-    setValute(data.data)
-  }
-  async function apiValuteRub(){
-    const response = await fetch(`${apiConfig.url}latest?apikey=${apiConfig.key}&base_currency=${valueRub}&currencies=RUB`)
-    const data = await response.json()
-    console.log(data.data.RUB)
-    setRub(data.data.RUB)
-    setShow(true)
-    if (valueRub === '') {
-      setShow(false)
-    }
-  }
-
   return (
-     <div className="app-container">
-       <h1 className="app-title">Конвертер валют</h1>
-
-       <div className="currency-block">
-         <label>
-           <input
-              type="text"
-              placeholder="Введите базовую валюту (USD, EUR...)"
-              value={valueBase}
-              onChange={changeValueBase}
-              maxLength={3}
-           />
-           <Button onClick={apiValute}>Найти</Button>
-           <Button onClick={() => setValute(null)} className="secondary">Скрыть</Button>
-         </label>
-
-         {valute && (
-            <ul>
-              {Object.entries(valute).map(([currency, rate]) => (
-                 <li key={currency} className={currency === 'RUB' ? 'highlight' : ''}>
-                   {currency}: {rate.toFixed(4)}
-                 </li>
-              ))}
-            </ul>
-         )}
-       </div>
-
-       <div className="currency-block">
-         <label>
-           <input
-              type="text"
-              placeholder="Введите валюту для конвертации в RUB"
-              value={valueRub}
-              onChange={changeValueRub}
-              maxLength={3}
-           />
-           <Button onClick={apiValuteRub}>Конвертировать</Button>
-         </label>
-
-         {show && rub !== null && (
-            <ul>
-              <li className="highlight">
-                {rub.toFixed(4)} RUB
-              </li>
-            </ul>
-         )}
-       </div>
-     </div>
+     <>
+     {/*<Component/>*/}
+       <ToDo/>
+     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,23 +7,23 @@
 //   )
 // }
 import type { ReactNode, MouseEvent } from 'react';
+import * as React from "react";
 
 interface ButtonProps {
   children: ReactNode;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLButtonElement>) => void;
 }
 
 export default function Button(
    {
      children,
-     onClick,
-     disabled = false
+      ...props
 }: ButtonProps) {
   return (
      <button
-        onClick={onClick}
-        disabled={disabled}
+        {...props}
      >
        {children}
      </button>
