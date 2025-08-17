3d8b35e79fa0cfa16e3f39e34b68940af2a4297b
Danil <hassie54men@icloud.com>
2025-08-17 23:55:27 +0300

    сделал что то наподобии туду, конвертер валют закоментировал на время

---
+++ src/components/ToDo/ToDo.tsx
@@ -0,0 +1,56 @@
+import Button from "../Button.tsx";
+import {useState} from "react";
+import * as React from "react";
+
+export default function ToDo() {
+
+  const [value, setValue] = useState<string>('')
+  const [todo, setTodo] = useState<string[]>([])
+
+  function addTodo() {
+    if (value.trim() !== '') {
+      setTodo([...todo, value])
+      setValue('')
+    }
+  }
+  function addTodoEnter(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      addTodo()
+    }
+  }
+
+  function clearTodos() {
+    setTodo([])
+  }
+
+  function deleteTodo(index: number){
+    setTodo(todo.filter((_, item) => item !== index))
+  }
+
+
+
+  return (
+     <div>
+       <label>
+         <Button onClick={clearTodos}>Очистить</Button>
+         <input
+            type="text"
+            placeholder='введите задачу'
+            value={value}
+            onChange={event => setValue(event.target.value)}
+            onKeyDown={addTodoEnter}
+         />
+         <Button onClick={addTodo}>Добавить</Button>
+       </label>
+       <ul>
+         {todo.map((item,index) => (
+            <>
+              <li key={index}>{item}</li>
+              <Button onClick={() => deleteTodo(index)}>Удалить</Button>
+            </>
+         ))
+         }
+       </ul>
+     </div>
+  )
+}
